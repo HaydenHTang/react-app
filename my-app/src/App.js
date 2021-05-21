b452d5a7e417afedc3f702a398e759fcdcb440fc
Hayden Tang <hayden.h.tang@megaintelligence.net>
2021-05-21 20:37:30 +0800

    implementing local state sample

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    age: 0
+  }
+
+  onAgeUp = () => {
+    this.setState({
+      ...this.state,
+      age: ++this.state.age
+    })
+  }
+
+  onAgeDown = () => {
+    this.setState({
+      ...this.state,
+      age: --this.state.age
+    })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>Age: <span>{this.state.age}</span></div>
+          <button onClick={this.onAgeUp}>Age Up</button>
+          <button onClick={this.onAgeDown}>Age Down</button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
