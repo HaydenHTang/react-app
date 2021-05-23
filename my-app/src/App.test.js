c47f86a2db920ce0506fcb4948baf2a320227094
Hayden Tang <hayden.h.tang@megaintelligence.net>
2021-05-23 23:07:07 +0800

    run simple test

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import reducer from './store/reducer'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const store = createStore(reducer);
+  render(<Provider store={store}>
+      <App /></Provider>)
+  const linkElement = screen.getByText(/Number Of User Created/i);
   expect(linkElement).toBeInTheDocument();
 });
